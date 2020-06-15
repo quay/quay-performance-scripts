5959fa0709120d88cb803b3a7053292b6d88247d
Joe Talerico <jtaleric@redhat.com>
2020-06-15 19:02:51 -0400

    Methods to load db with teams/users

--- quay_load.sh
+++ quay_load.sh
@@ -1,18 +1,53 @@
 #!/bin/bash
+set -x
 if [ "$#" -ne 2 ]; then
   echo "Usage: "
-  echo "	./script <URL> <TOKEN>"
+  echo "        ./script <URL> <TOKEN>"
   exit 1
 fi
 
 token=$2
 url=$1
 version="/api/v1"
+org=test
 
+# create_user <user>
 create_user() {
-  path="/organization/test/team/$1"
-  data='{"name":"$1","role":"member"}'
-  curl -H "Content-Type: application/json" -k -X PUT -d $data -H "Authorization: Bearer $2" ${3}${4}${path}
+  path="/superuser/users/"
+  data={\"email\":\"$1@test.com\",\"username\":\"$1\"}
+  curl -H "Content-Type: application/json" -k -X POST -d $data -H "Authorization: Bearer $token" ${url}${version}${path}
 }
 
-create_user test1 $token $url $version
+# create_team <team>
+create_team() {
+  path="/organization/test/team/$1"
+  data={\"name\":\"$1\",\"role\":\"member\"}
+  curl -H "Content-Type: application/json" -k -X PUT -d $data -H "Authorization: Bearer $token" ${url}${version}${path}
+}
+
+# add_user_to_team <team> <user>
+add_user_to_team() {
+  path="/organization/$org/team/$1/members/$2"
+  data={}
+  curl -H "Content-Type: application/json" -k -X PUT -d $data -H "Authorization: Bearer $token" ${url}${version}${path}
+}
+
+# add_team_to_repo <team> <repo>
+add_team_to_repo() {
+  path="/repository/$org/$1/permissions/team/$2"
+  data={\"role\":\"admin\"}
+  curl -H "Content-Type: application/json" -k -X PUT -d $data -H "Authorization: Bearer $token" ${url}${version}${path}
+}
+
+# create_repo <repo_name>
+create_repo() {
+  path="/repository"
+  data={\"description\":\"$1\",\"repo_kind\":\"image\",\"namespace\":\"${org}\",\"repository\":\"$1\",\"visibility\":\"public\"}
+  curl -H "Content-Type: application/json" -k -X POST -d $data -H "Authorization: Bearer $token" ${url}${version}${path}
+}
+
+create_team test_team6
+create_user test_user6
+create_repo test_repo6
+add_user_to_team test_team6 test_user6
+add_team_to_repo test_repo6 test_team6
