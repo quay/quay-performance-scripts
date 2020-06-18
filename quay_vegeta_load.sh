df93b43881be8881f276239e77465227a6f11e82
Joe Talerico <jtaleric@redhat.com>
2020-06-18 10:52:36 -0400

    Vegeta script complete

--- quay_vegeta_load.sh
+++ quay_vegeta_load.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
 if [ "$#" -ne 2 ]; then
   echo "Usage: "
   echo "        ./script <URL> <TOKEN>"
@@ -11,16 +10,16 @@ url=$1
 version="/api/v1"
 org=test
 password=password
-target_num=100
+target_num=1000
 rate=10
-prefix=ptest
+prefix=perf-test
 
 # create_user count
 create_user() {
   path="/superuser/users/"
   URL=${url}${version}${path}
   echo "+-----------------------+ Generating Users +-----------------------+"
-  /usr/bin/jq  --arg token $token -ncM '.=1 | while(. < '${1}'; .+1 ) | {method: "POST", url: "'$URL'", body: {email: ("'${prefix}'_user_" + (.|tostring) +"@test.com"),username: ("'${prefix}'_user_" + (.|tostring))}| @base64, "header":{"Authorization": ["Bearer " + $token], "Content-Type":["application/json"]}}' | ./vegeta attack -lazy -format=json -rate $rate -insecure | ./vegeta report
+  /usr/bin/jq  --arg token $token -ncM '.=1 | while(. < '${1}'; .+1 ) | {method: "POST", url: "'$URL'", body: {email: ("'${prefix}'_user_" + (.|tostring) +"@test.com"),username: ("'${prefix}'_user_" + (.|tostring))}| @base64, "header":{"Authorization": ["Bearer " + $token], "Content-Type":["application/json"]}}' | ./vegeta attack -lazy -format=json -rate $rate -insecure | ./vegeta report | tee create_user-performance.log
   echo "+---------------------+ End Generating Users +---------------------+"
 }
 
@@ -30,7 +29,7 @@ update_password() {
   path="/superuser/users"
   URL=${url}${version}${path}/${prefix}_user_
   echo "+-----------------------+ Update Passwords +-----------------------+"
-  /usr/bin/jq  --arg token $token -ncM '.=1 | while(. < '${1}'; .+1 ) | {method: "PUT", url: ("'${URL}'"+(.|tostring)), body: {password:"'$password'"}| @base64, "header":{"Authorization": ["Bearer " + $token], "Content-Type":["application/json"]}}' | ./vegeta attack -lazy -format=json -rate $rate -insecure | ./vegeta report
+  /usr/bin/jq  --arg token $token -ncM '.=1 | while(. < '${1}'; .+1 ) | {method: "PUT", url: ("'${URL}'"+(.|tostring)), body: {password:"'$password'"}| @base64, "header":{"Authorization": ["Bearer " + $token], "Content-Type":["application/json"]}}' | ./vegeta attack -lazy -format=json -rate $rate -insecure | ./vegeta report | tee update_password-performance.log
   echo "+---------------------+ End Update Passwords +---------------------+"
 }
 
@@ -39,7 +38,8 @@ create_team() {
   path=/organization/test/team/${prefix}_team_
   URL=${url}${version}${path}
   echo "+-----------------------+ Generating Teams +-----------------------+"
-  /usr/bin/jq  --arg token $token -ncM '.=1 | while(. < '${1}'; .+1 ) | {method: "PUT", url: ("'${URL}'"+(.|tostring)), body: {name: ("'${prefix}'_team_" + (.|tostring)),role:"member"}| @base64, "header":{"Authorization": ["Bearer " + $token], "Content-Type":["application/json"]}}' | ./vegeta attack -lazy -format=json -rate $rate -insecure | ./vegeta report
+  /usr/bin/jq  --arg token $token -ncM '.=1 | while(. < '${1}'; .+1 ) | {method: "PUT", url: ("'${URL}'"+(.|tostring)), body: {name: ("'${prefix}'_team_" + (.|tostring)),role:"member"}| @base64, "header":{"Authorization": ["Bearer " + $token], "Content-Type":["application/json"]}}' | ./vegeta attack -lazy -format=json -rate $rate -insecure | ./vegeta report | tee create_team-performance.log
+
   echo "+---------------------+ End Generating Teams +---------------------+"
 }
 
@@ -48,25 +48,32 @@ create_repo() {
   path="/repository"
   URL=${url}${version}${path}
   echo "+-----------------------+ Generating Repos +-----------------------+"
-  /usr/bin/jq  --arg token $token -ncM '.=1 | while(. < '${1}'; .+1 ) | {method: "POST", url: "'${URL}'", body: {description:"test",repo_kind:"image",namespace:"'$org'",repository: ("'${prefix}'_repo_" + (.|tostring)),visibility:"public"}| @base64, "header":{"Authorization": ["Bearer " + $token], "Content-Type":["application/json"]}}' | ./vegeta attack -lazy -format=json -rate $rate -insecure | ./vegeta report
+  /usr/bin/jq  --arg token $token -ncM '.=1 | while(. < '${1}'; .+1 ) | {method: "POST", url: "'${URL}'", body: {description:"test",repo_kind:"image",namespace:"'$org'",repository: ("'${prefix}'_repo_" + (.|tostring)),visibility:"public"}| @base64, "header":{"Authorization": ["Bearer " + $token], "Content-Type":["application/json"]}}' | ./vegeta attack -lazy -format=json -rate $rate -insecure | ./vegeta report | tee create_repo-performance.log
   echo "+---------------------+ End Generating Repos +---------------------+"
 }
 
-# add_user_to_team <team> <user>
+# add_user_to_team count
 add_user_to_team() {
-  path="/organization/$org/team/$1/members/$2"
-  data={}
-  curl -H "Content-Type: application/json" -k -X PUT -d $data -H "Authorization: Bearer $token" ${url}${version}${path}
+  path=/organization/$org/team/
+  URL=${url}${version}${path}
+  echo "+-----------------------+ Linking User to Team +-----------------------+"
+  /usr/bin/jq  --arg token $token -ncM '.=1 | while(. < '${1}'; .+1 ) | {method: "PUT", url: ("'${URL}''${prefix}'_team_"+(.|tostring)+"/members/'${prefix}'_user_"+(.|tostring)), body: {}| @base64, "header":{"Authorization": ["Bearer " + $token], "Content-Type":["application/json"]}}' | ./vegeta attack -lazy -format=json -rate $rate -insecure | ./vegeta report | tee add_user_to_repo-performance.log
+  echo "+---------------------+ End Linking User to Team +---------------------+"
 }
 
-# add_team_to_repo <team> <repo>
+# add_team_to_repo count
 add_team_to_repo() {
-  path="/repository/$org/$1/permissions/team/$2"
-  data={\"role\":\"admin\"}
-  curl -H "Content-Type: application/json" -k -X PUT -d $data -H "Authorization: Bearer $token" ${url}${version}${path}
+  path=/repository/$org/
+  URL=${url}${version}${path}
+  echo "+-----------------------+ Linking Repo to Team +-----------------------+"
+  /usr/bin/jq  --arg token $token -ncM '.=1 | while(. < '${1}'; .+1 ) | {method: "PUT", url: ("'${URL}''${prefix}'_repo_"+(.|tostring)+"/permissions/team/'${prefix}'_team_"+(.|tostring)), body: {role:"admin"}| @base64, "header":{"Authorization": ["Bearer " + $token], "Content-Type":["application/json"]}}' | ./vegeta attack -lazy -format=json -rate $rate -insecure | ./vegeta report | tee add_team_to_repo-performance.log
+  echo "+---------------------+ End Linking Repo to Team +---------------------+"
+
 }
 
 create_user $target_num
 update_password $target_num
 create_team $target_num
 create_repo $target_num
+add_user_to_team $target_num
+add_team_to_repo $target_num
