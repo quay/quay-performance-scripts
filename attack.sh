0b5c5803e8f9e9638a6c8bb894e13d15f9a15044
Joe Talerico <joe.talerico@gmail.com>
2020-06-22 07:18:56 -0400

    Moving things around a bit

--- attack.sh
+++ attack.sh
@@ -2,8 +2,8 @@
 
 set -u
 
-export TOKEN=opiqq6KJpCnn4YWqS4kkPku7pohjfzKX10EOGrUi
-export QUAY_URL=rook-quay-quay-openshift-quay.apps.rook43quay.perf-testing.devcluster.openshift.com
+export TOKEN=<Token>
+export QUAY_URL=<URL>
 export PARALLELISM=200
 export POD_COUNT=200
 export NUM_USERS=500
@@ -13,8 +13,8 @@ export NUM_TAGS=10
 kubectl create ns quay-perf
 kubectl delete cm load-script -n quay-perf --ignore-not-found
 kubectl create cm --from-file=build-script.sh load-script -n quay-perf
-kubectl apply -f role.yaml 
-kubectl apply -f rolebinding.yaml
-cat create-container-image-job.yaml | envsubst > newjob.yaml
+kubectl apply -f assets/role.yaml
+kubectl apply -f assets/rolebinding.yaml
+cat assets/create-container-image-job.yaml | envsubst > newjob.yaml
 kubectl apply -f newjob.yaml
 
--- quay_init.sh
+++ quay_init.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/bash 
+#!/usr/bin/bash
 
 #
 # This script assumes the Quay Operator has deployed Quay and we can modify the database.
 #
 
 quay_ns=openshift-quay
-db=$(oc get pods -n $quay_ns -l quay-enterprise-component=quay-database --no-headers -o name) 
-app=$(oc -n $quay_ns get pods -l quay-enterprise-component=app -o name) 
+db=$(oc get pods -n $quay_ns -l quay-enterprise-component=quay-database --no-headers -o name)
+app=$(oc -n $quay_ns get pods -l quay-enterprise-component=app -o name)
 
 # Copy over DB which has the app token generated
-oc -n $quay_ns cp ./quaydb $(echo $db | awk -F/ '{print $2}'):/tmp/database
+oc -n $quay_ns cp ./assets/quaydb $(echo $db | awk -F/ '{print $2}'):/tmp/database
 
 # Reset Database
 oc -n $quay_ns exec $db -- dropdb quay
