35adff416ca2620817169c531943cc30c35bd486
Joe Talerico <jtaleric@redhat.com>
2020-06-29 14:05:28 -0400

    Run Script
    Run Script updated to have v2 endpoint test

--- run-script.sh
+++ run-script.sh
@@ -1,17 +1,44 @@
 #!/usr/bin/bash -u
-set -x
 
-organization=test
 prefix=perf-test
-num_users=${NUM_USERS:-1000}
+num_users=${NUM_USERS:-30}
 rate=${RATE:-10}
 quay=${QUAY_URL}
-duration=${DURATION:-60}
-token=${TOKEN}
+duration=${DURATION:-10}
 pick=$((1 + RANDOM % ${num_users}))
 
-repo_tag_target=api/v1/repository/${organization}/${prefix}_repo_${pick}/tag/
+echo "Capture Token"
+token_10=$(curl -k -L --user ${prefix}_user_${pick}:password "https://${quay}/v2/auth?service=${quay}&scope=repository:${prefix}_user_${pick}/repo_10_tags:pull,push" | jq '.token' | sed -e 's/"//g')
+token_100=$(curl -k -L --user ${prefix}_user_${pick}:password "https://${quay}/v2/auth?service=${quay}&scope=repository:${prefix}_user_${pick}/repo_100_tags:pull,push" | jq '.token' | sed -e 's/"//g')
+token_500=$(curl -k -L --user ${prefix}_user_${pick}:password "https://${quay}/v2/auth?service=${quay}&scope=repository:${prefix}_user_${pick}/repo_500_tags:pull,push" | jq '.token' | sed -e 's/"//g')
+token_1000=$(curl -k -L --user ${prefix}_user_${pick}:password "https://${quay}/v2/auth?service=${quay}&scope=repository:${prefix}_user_${pick}/repo_1000_tags:pull,push" | jq '.token' | sed -e 's/"//g')
 
-echo "Running against tag endpoint"
-echo "GET https://${quay}/${repo_tag_target}" | vegeta attack -insecure -rate ${rate} -duration=${duration}s | tee report.data | vegeta report
+# Catalog
+echo "+-----------------------+ v2 _catalog +-----------------------+"
+URL=https://${quay}/v2/_catalog
+echo "GET $URL" | ./vegeta attack -rate $rate -duration ${duration}s -insecure -header "Authorization: Bearer $token_10" | ./vegeta report
+echo "+---------------------+ End v2 _catalog +---------------------+"
 
+# List Tags 10_tags
+echo "+-----------------------+ v2 repo with 10 tag +-----------------------+"
+URL=https://${quay}/v2/${prefix}_user_${pick}/repo_10_tags/tags/list
+echo "GET $URL" | ./vegeta attack -rate $rate -duration ${duration}s -insecure -header "Authorization: Bearer $token_10" | ./vegeta report
+echo "+---------------------+ End v2 repo with 10 tag +---------------------+"
+
+# List Tags 100_tags
+echo "+-----------------------+ v2 repo with 100 tag +-----------------------+"
+URL=https://${quay}/v2/${prefix}_user_${pick}/repo_100_tags/tags/list
+echo "GET $URL" | ./vegeta attack -rate $rate -duration ${duration}s -insecure -header "Authorization: Bearer $token_100" | ./vegeta report
+echo "+---------------------+ End v2 repo with 100 tag +---------------------+"
+
+# List Tags 500_tags
+echo "+-----------------------+ v2 repo with 500 tag +-----------------------+"
+URL=https://${quay}/v2/${prefix}_user_${pick}/repo_500_tags/tags/list
+echo "GET $URL" | ./vegeta attack -rate $rate -duration ${duration}s -insecure -header "Authorization: Bearer $token_500" | ./vegeta report
+echo "+---------------------+ End v2 repo with 500 tag +---------------------+"
+
+# List Tags 1000_tags
+echo "+-----------------------+ v2 repo with 1000 tag +-----------------------+"
+URL=https://${quay}/v2/${prefix}_user_${pick}/repo_1000_tags/tags/list
+echo "GET $URL" | ./vegeta attack -rate $rate -duration ${duration}s -insecure -header "Authorization: Bearer $token_1000" | ./vegeta report
+echo "+---------------------+ End v2 repo with 1000 tag +---------------------+"
