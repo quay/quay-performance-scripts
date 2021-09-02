05a2d209ef54434f6389316e16182141af33eb9b
parallels <sunanda.3094@gmail.com>
2021-09-02 11:47:54 -0400

    Replacing auth token with placeholder

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ class Settings(object):
     PODMAN_PASSWORD = 'password'
     PODMAN_HOST = 'localhost:8080'
     QUAY_HOST = 'http://www.localhost:8080'
-    AUTH_TOKEN = '7MFyH191ufNwhJBYfxB2fxFBJhURzEZIK8q4zmkL'
-    ROBOT_AUTH_TOKEN = 'YWRtaW4rbmV3cm9iOkhaQVYzRTZNT1VYVFJMSVY2SzRFUUVVTkFXM1c2QkZVM1ZMT0pCS1pXRzE0STdOT0FWN1o0TDdRTVlVWkRZVkg='
+
+    # Auth tokens
+    AUTH_TOKEN = '<auth-token>'
+    ROBOT_AUTH_TOKEN = '<robot-auth-token>'
 
     # Url Paths
     V2_AUTH = '/v2/auth'
