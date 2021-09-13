f2e7facceabded9a21baba196e57af418beb3468
parallels <sunanda.3094@gmail.com>
2021-09-13 17:20:09 -0400

    Randomizing container images for podman operations

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+import json
+import os
+
+
 class Settings(object):
     # Url Paths
     V2_AUTH = '/v2/auth'
     V1_CREATE_ORG = '/api/v1/organization/'
     V1_CREATE_REPO = '/api/v1/repository'
+
+    CONTAINER_IMAGES = json.loads(os.environ['CONTAINER_IMAGES'])
+    AUTH_TOKENS = json.loads(os.environ['AUTH_TOKENS'])
+    MAX_PULL_RETRIES = 3
--- testfiles/podman_operations.py
+++ testfiles/podman_operations.py
@@ -1,10 +1,29 @@
-from locust import User, task, tag
+from locust import HttpUser, User, task, tag
 from subprocess import run
 
 from utils import trigger_event
+from config import Settings
 import os
+import random
 
-class PodmanUser(User):
+counter = 1
+
+
+class PodmanUser(HttpUser):
+
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.chosen_img = random.choice(Settings.CONTAINER_IMAGES)
+        self.image_tag = 'latest'
+        self.image_name = self.chosen_img
+        if ':' in self.chosen_img:
+            self.image_name, self.image_tag = self.chosen_img.split(':')[0].split('/')[-1], self.chosen_img.split(':')[-1]
+
+        self.upstream_image = f"{os.environ['CONTAINER_IMAGE_HOST']}/{self.chosen_img}"
+        global counter
+        self.local_image = f"{os.environ['PODMAN_HOST']}/{os.environ['PODMAN_USERNAME']}/{self.image_name+str(counter)}:{self.image_tag}"
+        counter += 1
+        self.retries = 0
 
     def on_start(self):
         pass
@@ -24,14 +43,25 @@ class PodmanUser(User):
         """
             Pushing image via podman.
         """
-        cmd = f"podman login {os.environ['PODMAN_HOST']} -u {os.environ['PODMAN_USERNAME']} -p {os.environ['PODMAN_PASSWORD']} --tls-verify=false"
-        run(cmd, shell=True, capture_output=True)
+        try:
+            cmd = f"podman login {os.environ['PODMAN_HOST']} -u {os.environ['PODMAN_USERNAME']} -p {os.environ['PODMAN_PASSWORD']} --tls-verify=false"
+            run(cmd, shell=True, capture_output=True)
 
-        cmd = f"podman pull quay.io/alecmerdler/bad-image:critical --tls-verify=false"
-        run(cmd, shell=True, capture_output=True)
+            while self.retries < Settings.MAX_PULL_RETRIES:
+                cmd = f"podman pull {self.upstream_image} && \
+                        podman tag {self.upstream_image} {self.local_image} && \
+                        podman push {self.local_image} --tls-verify=false && \
+                        podman rmi {self.upstream_image}"
+                r = run(cmd, shell=True, capture_output=True)
+                if r.returncode == 0:
+                    break
 
-        cmd= f"podman tag quay.io/alecmerdler/bad-image:critical {os.environ['PODMAN_HOST']}/admin/bad-image:critical"
-        run(cmd, shell=True, capture_output=True)
+                self.retries += 1
+                continue
 
-        cmd = f"podman push {os.environ['PODMAN_HOST']}/admin/bad-image:critical --tls-verify=false"
-        return run(cmd, shell=True, capture_output=True)
+            self.retries = 0
+            return r
+        except RuntimeError:
+            pass
+        except Exception as err:
+            return err
